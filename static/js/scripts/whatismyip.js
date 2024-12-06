881770d177f6fa6a046877e6a57a9c88858cce73
lingeaniket <linge.aniket.10@gmail.com>
2024-12-06 14:20:31 +0530

    change in whatismyip.js file in dive alinecenter all code 1 to 23

--- static/js/scripts/whatismyip.js
+++ static/js/scripts/whatismyip.js
@@ -1,18 +1,23 @@
-function onSubmit(){fetch("https://api.ipify.org?format=json").then((response)=>response.json()).then((data)=>{const ip=data.ip;fetch(`/whatis-my-ip-data?ip=${ip}`).then((response)=>response.json()).then((data)=>{const whatismyipdata=document.getElementById("whatismyipdata");result=data;whatismyipdata.innerHTML=`
+function onSubmit() {
+    fetch("https://api.ipify.org?format=json").then((response) => response.json()).then((data) => {
+        const ip = data.ip; fetch(`/whatis-my-ip-data?ip=${ip}`).then((response) => response.json()).then((data) => {
+            const whatismyipdata = document.getElementById("whatismyipdata"); result = data; whatismyipdata.innerHTML = `
     <section class="about">
         <div class="container" data-aos="fade-up">
             <div class="row gx-0">
-                <div class="col-lg-12 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
-                    <div class="content justify-content-center text-center">
-                        <p>My Public IPv4:<b>${result.ipv4}</b></p>
-                        <p>My Public IPv6:<b>Not Detected</b></p>
-                        <p>My ISP:<b>${result.address.org}</b></p>
-                        <p>
+                <div class="col-lg-12 d-flex flex-column justify-content-center " data-aos="fade-up" data-aos-delay="200">
+                    <div class="content d-flex flex-wrap fs-6 justify-content-center text-center">
+                        <div class="w-100 p-2">My Public IPv4:<b>${result.ipv4}</b></div>
+                        <div class="w-100 p-2">My Public IPv6:<b>Not Detected</b></div>
+                        <div class="w-100 p-2">My ISP:<b>${result.address.org}</b></div>
+                        <div class="w-100">
                             My Location:<b>${result.address.city}, ${result.address.state}, ${result.address.country}</b>
-                        </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>`})})}
-function onload(){document.getElementById("loader").style.display="block";onSubmit()}
+    </section>`})
+    })
+}
+function onload() { document.getElementById("loader").style.display = "block"; onSubmit() }
